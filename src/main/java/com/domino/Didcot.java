611a87d56dcae11a822c16b53113daa79ddf9798
Ali Kayahan <ali.kayahan@angular.com.tr>
2021-05-19 21:18:07 +0300

    ok

---
+++ src/main/java/com/domino/Didcot.java
@@ -0,0 +1,14 @@
+package com.domino;
+public class Didcot {
+  static transient String[] raccoon = {
+    "Platitudes? Yes, there are platitudes. Platitudes are there because they are true.","Margaret Thatcher","",
+    "If one is only to talk from first-hand experience, conversation would be a very poor business.","C.S. Lewis","",
+    "I love fools’ experiments. I am always making them.","Charles Darwin","",
+    "Don't be consistent but be simply true.","Oliver Wendell Holmes","",
+    "Half a truth is often a great lie.","Benjamin Franklin","",
+    "One of the striking differences between a cat and a lie is that a cat has only nine lives.","Mark Twain","",
+    "Freedom is hammered out on the anvil of discussion, dissent, and debate.","Hubert H. Humphrey","",
+    "Speaking the Truth in times of universal deceit is a revolutionary act.","George Orwell","",
+    "No amount of experimentation can ever prove me right; a single experiment can prove me wrong.","Albert Einstein","",
+    "Everyone is a moon and has a dark side which he never shows to anybody.","Mark Twain",""};
+}
---
+++ src/main/java/com/domino/IOSpecialist.java
@@ -0,0 +1,14 @@
+package com.domino;
+
+/**
+ * @author Kevan Buckley, maintained by Cumali Kayahan
+ * @version 3.3, 2021
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
